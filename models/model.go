c892e6f293ab70e006f0a0ffa97950d122b2c5ca
albimcleod <albimcleod@gmail.com>
2017-09-26 20:09:54 +0930

    change to models

---
+++ models/model.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type Model struct {
+	ID        uint       `gorm:"primary_key" json:"id,omitempty"`
+	CreatedAt time.Time  `json:"createdAt,omitempty"`
+	UpdatedAt time.Time  `json:"updatedAt,omitempty"`
+	DeletedAt *time.Time `sql:"index" json:"deletedAt,omitempty"`
+}
